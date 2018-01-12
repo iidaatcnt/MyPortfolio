3c97c752193be16038fb70f5e7da475e7078862c
Masa Iida <iida.m@cntsv.jp>
2018-01-13 01:46:10 +0900

    update css/styles.css index.html js/main.js

--- cube/js/main.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-
-  var open = document.getElementById('open');
-  var close = document.getElementById('close');
-  var modal = document.getElementById('modal');
-  var mask = document.getElementById('mask');
-})();
