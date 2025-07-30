009bb5dd55e1ec5879decc4c4c7b8298b7f96dc8
iidatcnt <miidacnt@gmail.com>
2025-07-30 16:38:51 +0900

    Remove cube-timer directory (moved to separate repository)

--- cube-timer/js/main.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-
-  var timer = document.getElementById('timer');
-  var start = document.getElementById('start');
-  var stop  = document.getElementById('stop');
-  var reset = document.getElementById('reset');
-})();
